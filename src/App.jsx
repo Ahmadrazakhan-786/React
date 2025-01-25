6470e6387252ccdc1136cbe8856c248f71b341c1
Ahmadraza <razakhan.srt786@gmail.com>
2025-01-25 22:33:05 +0530

    init commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,9 @@ function App(){
   return (
     <div className="App">
       <div>Videos</div>
-      <Video  title="React Js Tutorial" views="10k" time="1 year ago" channel="Coder Dost"></Video>
-      <Video title="Node Js Tutorial " views="100k" time="1 month ago" channel="Coder Dost"></Video>
-      <Video title="Mongo DB Tutorial " views="1M" time="2 month ago" channel="Coder Dost"></Video>
+      <Video verified={true} title="React Js Tutorial" views="10k" time="1 year ago" channel="Coder Dost"></Video>
+      <Video verified={false} title="Node Js Tutorial " views="100k" time="1 month ago" channel="Coder Dost"></Video>
+      <Video verified={false} title="Mongo DB Tutorial " views="1M" time="2 month ago" channel="Coder Dost"></Video>
     </div>
     
   ) 
--- src/Components/Video.jsx
+++ src/Components/Video.jsx
@@ -1,13 +1,20 @@
 import './Video.css';
 
-function Video({title,channel,views,time}){
+function Video({title,channel,views,time,verified}){
+    let channelJSX;
+    if(verified){
+      channelJSX = <div className="channel">{channel} ✅</div>
+    } else{
+        channelJSX = <div className="channel">{channel} </div>
+    }
+
     return ( 
     <div className="container">
         <div className="pic">
-        <img src="https://loremflickr.com/190/90" alt="" />
+        <img src="https://pixlr.com/images/generator/how-to-generate.webp" alt="" />
         </div>
         <div className="title">{title}</div>
-        <div className="channel">{channel}</div>
+        {channelJSX}
         <div className="views">
             {views} views <span>.</span> {time}
         </div>
