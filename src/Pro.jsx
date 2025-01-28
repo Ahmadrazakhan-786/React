4c7b5088ca913041dfd76ff9f2d69d6313f79d2c
Ahmadraza <razakhan.srt786@gmail.com>
2025-01-28 11:26:23 +0530

    init commit

--- src/Pro.jsx
+++ src/Pro.jsx
@@ -1,16 +1,31 @@
+import { useState } from 'react';
 import './Pro.css';
 const Pro = ()=>{
 
-    const user = "ahmad";
-    const abc = ()=>{
-        console.log("hello");
-    }
-    abc();
+// const [a, setA] = useState(10);
+
+// const changeA =()=>{
+//     console.log('chal gya bhai');
+//     setA('ark');
+// }
+
+const [num, setnum] = useState(0); // hooks: variable of react
+
+
 
     return (
         <div>
-            <h1>Username is {user} </h1>
-            <button onClick={abc}>Change User</button>
+            {/* <h1>Username is {user} </h1> */}
+            {/* <button onClick={ChangeUser}>Change User</button> */}
+            {/* <h1>value of a is {a}</h1>
+            <button onClick={changeA}>Changed A</button> */}
+            <h3>Number is {num}</h3>
+            <button onClick={function(){
+                setnum(num+15);
+            }}>Increment</button>
+            <button onClick={function(){
+                setnum(9);
+            }}>Decrement</button>
         </div>
     )
 }
