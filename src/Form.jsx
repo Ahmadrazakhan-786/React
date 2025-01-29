cbd78684738bddc5019e3d70d5cb358013e2d2b9
Ahmadraza <razakhan.srt786@gmail.com>
2025-01-29 14:43:35 +0530

    two way binding commit

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,17 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Form = ()=>{
+
     const submitHandler = (e)=>{
         e.preventDefault(e); // it stops the page reloading behaviour
-        console.log("submitted");
+        console.log(username);
+        setusername('');
     }
+
+    const [username, setusername] = useState('')
 
     return(
         <div>
            <form onSubmit={(e)=>{
-            submitHandler(e)
+            submitHandler(e);
            }}>
-            <input className="px-4 py-3 text-xl m-5 rounded " type="text" placeholder="Enter your name" />
+            <input 
+            value={username}
+            onChange={(e)=>{
+                setusername(e.target.value);
+            }}
+            className="px-4 py-3 text-xl m-5 rounded " 
+            type="text" 
+            placeholder="Enter your name" 
+            />
             <button className="px-4 py-3 text-white m-5 text-xl font-semibold bg-emerald-600 rounded">Submit</button>
            </form>
         </div>
