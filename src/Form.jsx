080c50b2ab3e28387daf7bcf00031e9bf835d371
Ahmadraza <razakhan.srt786@gmail.com>
2025-01-29 09:00:47 +0530

    prevent default commit

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,8 +1,16 @@
 import React from "react";
+
 const Form = ()=>{
+    const submitHandler = (e)=>{
+        e.preventDefault(e); // it stops the page reloading behaviour
+        console.log("submitted");
+    }
+
     return(
         <div>
-           <form >
+           <form onSubmit={(e)=>{
+            submitHandler(e)
+           }}>
             <input className="px-4 py-3 text-xl m-5 rounded " type="text" placeholder="Enter your name" />
             <button className="px-4 py-3 text-white m-5 text-xl font-semibold bg-emerald-600 rounded">Submit</button>
            </form>
