a9d5f61592d8914bcad23763fd38d2536ac4414f
Ahmadraza <razakhan.srt786@gmail.com>
2025-02-05 19:20:06 +0530

    init commit

--- src/Api.jsx
+++ src/Api.jsx
@@ -1,8 +1,16 @@
+import axios from "axios"
+
 const Api = ()=>{
 
+    const getData = ()=>{
+        console.log("hello")
+        const response =  axios.get('https://piscum.photos/v2/list')
+        console.log(response)
+    }
     return(
-        <div>
-            <h1>hiii</h1>
+        <div className="p-10">
+            <button onClick={getData} className="bg-teal-700 text-white font-semibold text-2xl px-7 py-4 rounded active:scale-90">Get Data</button>
+            <div className="p-5 mt-5 bg-gray-950">hello</div>
         </div>
     )
 }
