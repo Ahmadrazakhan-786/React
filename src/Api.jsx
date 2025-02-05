5cb4800df16621fa49b204bf4755de07fbd215db
Ahmadraza <razakhan.srt786@gmail.com>
2025-02-05 23:22:28 +0530

    init commit

--- src/Api.jsx
+++ src/Api.jsx
@@ -1,16 +1,24 @@
 import axios from "axios"
+import { useState } from "react"
 
 const Api = ()=>{
 
-    const getData = ()=>{
-        console.log("hello")
-        const response =  axios.get('https://piscum.photos/v2/list')
-        console.log(response)
+    const [data, setData] = useState([])
+
+    const getData = async ()=>{
+        const response = await axios.get('https://picsum.photos/v2/list')
+        setData(response.data)
     }
     return(
         <div className="p-10">
             <button onClick={getData} className="bg-teal-700 text-white font-semibold text-2xl px-7 py-4 rounded active:scale-90">Get Data</button>
             <div className="p-5 mt-5 bg-gray-950">hello</div>
+            {data.map(function(elem,idx){
+                return <div key={idx} className="bg-gray-50 text-black flex items-center justify-between w-full px-7 py-6 rounded mb-3">
+                    <img className="h-40" src={elem.download_url} alt="" />
+                    <h1>{elem.author}</h1>
+                </div>
+            })}
         </div>
     )
 }
