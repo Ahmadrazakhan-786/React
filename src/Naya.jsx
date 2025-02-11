e1ea44c64bdcb65f3c38ec02624ad0d4c149eae5
Ahmadraza <razakhan.srt786@gmail.com>
2025-02-11 22:18:01 +0530

    hooks commit

--- src/Naya.jsx
+++ src/Naya.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
 
 const Naya = () => {
+
+  let counter = 15
+
+  const addvalue = ()=>{
+    console.log("clicked",Math.random())
+    counter += 1
+  }
+
+  const removevalue = () =>{
+    console.log("value removed")
+  }
   return (
-    <div className='text-xl'>Naya</div>
+    <>
+    <h1>Chai aur react</h1>
+    <h2>Counter values : {counter}</h2>
+    
+    <button onClick={addvalue}>Add value</button>
+    <br />
+    <button onClick={removevalue}>Remove value</button>
+    </>
+
   )
 }
 
