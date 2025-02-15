586d0ff54324b87e706fb76ba1dc06948c3ed562
Ahmadraza <razakhan.srt786@gmail.com>
2025-02-15 15:47:23 +0530

    password generator

--- src/Password.jsx
+++ src/Password.jsx
@@ -15,10 +15,29 @@ const Password = () => {
         if(numAllowed) str += "0123456789"
         if(charAllowed) str += "!@#$%^&*-_+=[]{}`~"
 
+        for(let i=1; i<=Array.length;i++){
+          let char = Math.floor(Math.random() * str.length + 1)
+          pass = str.charAt(char)
+        }
+
+        setpassword(pass)
+
     }, [length,numAllowed,charAllowed,setpassword])
   return (
     <>
-    <h1 className='text-10xl text-center mt-10 '>Password Generator</h1>
+    <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-700 '>
+      <h1 className='text-white text-center my-3 '>Password Generator</h1>
+      <div className='flex shadow rounded-lg overflow-hidden mb-4'>
+        <input 
+        type="text" 
+        value={password}
+        className='outline-none w-full py-1 px-3'
+        placeholder='password'
+        readOnly
+
+        />
+      </div>
+    </div>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css';
 // import App from './App.jsx'
-import Pro from './Pro.jsx';
-import Form from './Form.jsx'
-import Web from './Web.jsx';
-import Api from './Api.jsx';
-import UserContext from './context/UserContext.jsx';
-import Naya from './Naya.jsx';
-import Color from './Color.jsx';
+// import Pro from './Pro.jsx';
+// import Form from './Form.jsx'
+// import Web from './Web.jsx';
+// import Api from './Api.jsx';
+// import UserContext from './context/UserContext.jsx';
+// import Naya from './Naya.jsx';
+// import Color from './Color.jsx';
 import Password from './Password.jsx';
 
 // import { BrowserRouter } from 'react-router-dom';
