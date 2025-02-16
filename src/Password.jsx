198d5bcd770aa0b56e166e12c4b62adc39c6249a
Ahmadraza <razakhan.srt786@gmail.com>
2025-02-16 19:56:24 +0530

    password generator

--- src/Password.jsx
+++ src/Password.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useState, useCallback } from 'react'
+import { useState, useCallback, useEffect, useRef } from 'react'
 
 const Password = () => {
 
@@ -8,6 +8,9 @@ const Password = () => {
     const [charAllowed, setcharAllowed] = useState(false)
     const [password, setpassword] = useState("")
 
+    // useRef hook
+    const passwordRef = useRef(null)
+
     const passwordGenerator = useCallback(() =>{
         let pass = ""
         let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
@@ -15,14 +18,25 @@ const Password = () => {
         if(numAllowed) str += "0123456789"
         if(charAllowed) str += "!@#$%^&*-_+=[]{}`~"
 
-        for(let i=1; i<=Array.length;i++){
+        for(let i=1; i<=length;i++){
           let char = Math.floor(Math.random() * str.length + 1)
-          pass = str.charAt(char)
+          pass += str.charAt(char)
         }
 
         setpassword(pass)
 
     }, [length,numAllowed,charAllowed,setpassword])
+
+    const copyPasswordToClip = useCallback(()=> {
+      passwordRef.current?.select();
+      passwordRef.current?.setSelectionRange(0,999)
+      window.navigator.clipboard.writeText(password)
+    }, [password])
+
+    useEffect(()=> {
+      passwordGenerator()
+    }, [length,numAllowed,charAllowed,passwordGenerator])
+
   return (
     <>
     <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-700 '>
@@ -34,9 +48,11 @@ const Password = () => {
         className='outline-none w-full py-1 px-3'
         placeholder='password'
         readOnly
+        ref={passwordRef}
 
         />
-        <button className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'>copy</button>
+        <button onClick={copyPasswordToClip}
+        className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'>copy</button>
       </div>
       <div className='flex text-sm gap-x-2'>
         <div className='flex items-center gap-x-1'>
