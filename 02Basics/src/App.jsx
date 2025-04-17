a0bee644c5dce166db1fb5286b2e6899fd65d105
Ahmadraza <razakhan.srt786@gmail.com>
2025-04-17 22:50:04 +0530

    use navigate

--- 02Basics/src/App.jsx
+++ 02Basics/src/App.jsx
@@ -4,6 +4,10 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import InputBox from './Components/InputBox'
 import DisplayBox from './Components/DisplayBox'
+import { BrowserRouter as Router, Routes, Route, Link, useNavigate } from 'react-router-dom';
+import Home from './Pages/Home'
+import About from './Pages/About'
+import Contact from './Pages/Contact'
 
 function App() {
     // const [text, settext] = useState('')
@@ -15,14 +19,40 @@ function App() {
      <InputBox text={text} settext={settext}/>
      <DisplayBox text={text}/> */}
 
-     <Router>
+
+
+     {/* <Router>
       <div>
-        {/* navigation link */}
+        
         <nav>
-          
+         <Link to="/">Home</Link>  |{" "}
+         <Link to="/about">About</Link>  |{" "}
+         <Link to="/contact">Contact</Link>
+
         </nav>
+
+         <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/about" element={<About/>}/>
+          <Route path="/contact" element={<Contact/>}/>
+         </Routes>
       </div>
-     </Router>
+     </Router> */}
+
+      <Router>
+      <nav>
+         <Link to="/">Home</Link>  |{" "}
+         <Link to="/about">About</Link>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/about" element={<About/>}/>
+         
+         </Routes>
+
+      </Router>
+
     </>
   )
 }
--- 02Basics/src/Pages/Home.jsx
+++ 02Basics/src/Pages/Home.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
+
+  const navigate = useNavigate();
+
+  const gotoAbout = () =>{
+    navigate('/about');
+  }
+
   return (
     <div>
         <h2>This is Home Page</h2>
+        <button onClick={gotoAbout}>Go to About Page</button>
     </div>
   )
 }
