27d378a859b516940345bbf78b5e603ed57928c6
Ahmadraza <razakhan.srt786@gmail.com>
2025-04-25 22:09:33 +0530

    home section

--- 02Basics/src/Pages/Homes.jsx
+++ 02Basics/src/Pages/Homes.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Homes = () => {
   return (
     <div>
-        <h2>Welcome to Home Page</h2>
+        <h2>Welcome to my Home Page</h2>
         <Link to="/dashboard/profile" className='text-red-400'>Go to Profile</Link><br />
       <Link to="/dashboard/settings" className='text-emerald-500'>Go to Settings</Link>
     </div>
