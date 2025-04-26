07a71924b5430564c6dd967195fe12ad84b9eacb
Ahmadraza <razakhan.srt786@gmail.com>
2025-04-26 21:42:36 +0530

    new commit

--- 02Basics/src/Components/Dashboard.jsx
+++ 02Basics/src/Components/Dashboard.jsx
@@ -4,7 +4,7 @@ import {Link, Outlet } from 'react-router-dom'
 const Dashboard = () => {
   return (
     <div>
-        <h2 className='text-fuchsia-600'>Dashboard</h2>
+        <h2 className='text-fuchsia-600'> My Dashboard</h2>
 
         <nav>
             <Link to="profile" className='text-violet-600'>Profile</Link> | {" "}
