8fc32491be3a3d0d53c82035e99269afbccce27c
Ahmadraza <razakhan.srt786@gmail.com>
2025-04-30 22:45:53 +0530

    new commit

--- 02Basics/src/Components/Dashboard.jsx
+++ 02Basics/src/Components/Dashboard.jsx
@@ -7,7 +7,7 @@ const Dashboard = () => {
         <h2 className='text-fuchsia-600'> My Dashboard</h2>
 
         <nav>
-            <Link to="profile" className='text-violet-600'>Profile</Link> | {" "}
+            <Link to="profile" className='text-violet-600'>Profile Section</Link> | {" "}
             <Link to="settings" className='text-violet-700'>Settings</Link>
         </nav>
         <hr />
