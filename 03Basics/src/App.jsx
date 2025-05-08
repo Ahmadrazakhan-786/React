fefa4cd2b58a7f46aa7ea64976400e62493c0c7e
Ahmadraza <razakhan.srt786@gmail.com>
2025-05-08 15:43:24 +0530

    custom hooks

---
+++ 03Basics/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <h1 className='text-2xl text-red-600'>hii ahmad</h1>
+    </>
+  )
+}
+
+export default App
