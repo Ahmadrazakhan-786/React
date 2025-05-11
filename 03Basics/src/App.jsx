853cf3a6144efe091b5bc6cff106cbcb3960d717
Ahmadraza <razakhan.srt786@gmail.com>
2025-05-11 22:01:47 +0530

    new commit

--- 03Basics/src/App.jsx
+++ 03Basics/src/App.jsx
@@ -9,10 +9,6 @@ function App() {
 
   return (
     <>
-    {/* <div className='flex flex-col items-center justify-center h-screen'>
-     <h1 className='text-2xl text-red-600'>hii ahmad</h1>
-     <h2 className='bg-emerald-500 px-4 py-2 rounded mt-4 hover:bg-blue-500'>ARK</h2>
-    </div> */}
 
     <TimerComp />
     </>
--- 03Basics/src/Components/TimerComp.jsx
+++ 03Basics/src/Components/TimerComp.jsx
@@ -6,7 +6,7 @@ const TimerComp = () => {
     const count = usetimer();
   return (
     <div className='flex flex-col items-center justify-center h-screen bg-gray-400'>
-        <h2 className='text-2xl font-bold mb-4 text-black'>⏱ Timer Using Custom Hook</h2>
+        <h2 className='text-3xl font-bold mb-4 text-black'>⏱ Timer Using Custom Hook</h2>
         <p className='bg-blue-400 px-4 py-2 rounded font-semibold hover:bg-emerald-500'>Seconds passed : {count}</p>
     </div>
   )
