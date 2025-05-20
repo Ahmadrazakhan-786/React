4b086accb7a996438f8ba8d329d720e6cf8edb62
Ahmadraza <razakhan.srt786@gmail.com>
2025-05-20 20:43:39 +0530

    AXIOS

--- 03Basics/src/App.jsx
+++ 03Basics/src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import TimerComp from './Components/TimerComp'
 import Users from './Components/Users'
+import Movies from './Pages/Movies'
 
 function App() {
   
@@ -13,7 +14,9 @@ function App() {
 
     {/* <TimerComp /> */}
 
-    <Users />
+    {/* <Users /> */}
+
+    <Movies />
     </>
   )
 }
