c2c4f0c4b8598d83c056fc77504a6bec873ec403
Ahmadraza <razakhan.srt786@gmail.com>
2025-05-22 23:24:22 +0530

    new commit

--- 03Basics/src/Pages/Movies.jsx
+++ 03Basics/src/Pages/Movies.jsx
@@ -29,9 +29,9 @@ const Movies = () => {
    <div>
     <ul>
       {
-        data.map(() =>{
-          
-        })
+        data.map((user=>
+          <li key={user.id}>{user.name}</li>
+        ))
       }
     </ul>
    </div>
