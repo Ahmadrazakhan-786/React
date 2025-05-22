996e7f86c7f6c10d1d482a45bf07f9302452a2d7
Ahmadraza <razakhan.srt786@gmail.com>
2025-05-22 12:59:25 +0530

    api handling

--- 03Basics/src/Pages/Movies.jsx
+++ 03Basics/src/Pages/Movies.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import React, { useEffect , useState} from 'react'
+import axios from "axios";
+
 
 const Movies = () => {
+
+  const [data, setdata] = useState([])
+
+  const API = 'https://jsonplaceholder.typicode.com/users';
+  
+  const getuserData = async() => {
+      try {
+        const response =  await axios.get(API);
+        console.log(response);
+        setdata(response);
+      } catch (error) {
+        console.log(error)
+      }
+  }
+
+  useEffect(() => {
+     getuserData();
+  }, [])
+  
+  
+
   return (
-    <div>Upcoming movies..</div>
+
+   <div>
+    <ul>
+      {
+        data.map(() =>{
+          
+        })
+      }
+    </ul>
+   </div>
+    
   )
 }
 
