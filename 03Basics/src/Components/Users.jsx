20f1442026248ff117286dd080aabc8de774491d
Ahmadraza <razakhan.srt786@gmail.com>
2025-05-27 21:51:37 +0530

    new commit

--- 03Basics/src/Components/Users.jsx
+++ 03Basics/src/Components/Users.jsx
@@ -5,7 +5,7 @@ const Users = () => {
     const {data, error, loading} = useFetch('https://jsonplaceholder.typicode.com/users');
   return (
     <div className='flex flex-col items-center justify-center h-screen'>
-        <h2 className='text-2xl font-bold mb-4'>📦 Users Lists:</h2>
+        <h2 className='text-2xl font-bold mb-4'>📦 Users Lists:-</h2>
 
         {loading && <p className='text-green-500'>Loading...</p>}
         {error && <p className='text-red-500'>Error: {error}</p>}
